63deaa3190c06162ac8518654fff63fc78f63a9e
Parthiban Subramanian <Parthiban@colorado.getebs.com>
2017-04-06 18:01:18 +0530

    New Changes. Email ID Changed

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+    $(".datepicker").datepicker({
+        showOn: "button",
+        changeMonth:true,
+        changeYear:true,
+        dateFormat:"mm/dd/yy",
+        yearRange: "1980:1997",
+        buttonImage: "https://cdn3.iconfinder.com/data/icons/computers-programming/512/datepicker-256.png"
+    });
+
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#app_photo').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#photo").change(function(){
+        readURL(this);
+    });
+});
+
+$(function() {
+    $("#submit").click(function() {
+        if($("#agree:checked").is(":checked")){
+            sendMail();
+        }
+        else{
+            alert("Please Agree Our Conditions");
+        }
+
+        function sendMail() {
+            var data = {
+            typeofcourse: $(".typeofcourse:checked").val(),
+            name: $("#name").val(),
+            email: $("#email").val(),    
+            phone: $("#phone").val(),        
+            dob: $("#dob").val(),
+            gender: $(".gender:checked").val(),
+            fathername:$("#fathername").val(),
+            fatherno:$("#fatherno").val(),
+            fatheremail:$("#fatheremail").val(),
+            mothername:$("#mothername").val(),
+            motherno:$("#motherno").val(),
+            motheremail:$("#motheremail").val(),
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: data,
+            success: function(){
+                $('.success').fadeIn(1000);
+            }
+        });
+        return false;        
+    }        
+});
+});
+    
---
+++ mail.php
@@ -0,0 +1,54 @@
+<?php
+if($_POST){
+	$typeofcourse = $_POST['typeofcourse'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+	$phone = $_POST['phone'];	
+	$dob = $_POST['dob'];
+    $gender = $_POST['gender'];	
+	$fathername = $_POST['fathername'];
+	$fatherno = $_POST['fatherno'];	
+	$fatheremail = $_POST['fatheremail'];	
+	$mothername = $_POST['mothername'];	
+	$motherno = $_POST['motherno'];	
+	$motheremail = $_POST['motheremail'];	
+
+			//send email
+
+			$pattern = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i"; 
+            if (preg_match($pattern, trim(strip_tags($email)))) { 
+                $cleanedFrom = trim(strip_tags($email)); 
+            } else { 
+                return "The email address you entered was invalid. Please try again!"; 
+            } 
+
+			$message = '<html><body><table rules="all" style="border: 1px solid #9E9E9E;width: 500px;" cellpadding="10">';
+			$message .= "<tr><td colspan='2' style='text-align: center;'><strong>Applicant Details</strong></td></tr>";			
+			$message .= "<tr><td style='background: #ddd;'><strong>Name</strong></td><td>" . strip_tags($name) . "</td></tr>";
+			$message .= "<tr><td style='background: #ddd;'><strong>Email</strong></td><td>" . strip_tags($email) . "</td></tr>";
+			$message .= "<tr><td style='background: #ddd;'><strong>Phone</strong></td><td>" . strip_tags($phone) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>DOB</strong></td><td>" . strip_tags($dob) . "</td></tr>";
+			$message .= "<tr><td style='background: #ddd;'><strong>Gender</strong></td><td>" . strip_tags($gender) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Father Name</strong></td><td>" . strip_tags($fathername) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Father's No.</strong></td><td>" . strip_tags($fatherno) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Father's Mail</strong></td><td>" . strip_tags($fatheremail) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Mother Name</strong></td><td>" . strip_tags($mothername) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Mother's No.</strong></td><td>" . strip_tags($motherno) . "</td></tr>";	
+			$message .= "<tr><td style='background: #ddd;'><strong>Mother's Mail</strong></td><td>" . strip_tags($motheremail) . "</td></tr>";				
+			$message .= "<tr><td style='background: #ddd;'><strong>Type Of Course</strong></td><td>" . strip_tags($typeofcourse) . "</td></tr>";			
+			$message .= "</table></body></html>";
+
+			$to = 'asrafnaveen488@gmail.com';			
+			$subject = 'New Application';			
+			$headers = "From: " . $cleanedFrom . "\r\n";
+			$headers .= "Reply-To: ". strip_tags($email) . "\r\n";
+			$headers .= "MIME-Version: 1.0\r\n";
+			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+            if (mail($to, $subject, $message, $headers)) {
+              echo 'Your message has been sent.';
+            } else {
+              echo 'There was a problem sending the email.';
+            }
+}
+?>
